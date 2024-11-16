e2a5ed1362367f2a68374512781b832ee7607a47
aakashstha1 <aakash.cerestha90@gmail.com>
2024-11-16 19:21:54 +0545

    first commit

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <nav>NAVBAR</nav>
+      <main className="min-h-screen max-w-screen-2xl mx-auto px-4 py-6 font-primary">
+        <Outlet />
+      </main>
+      <footer>Footer</footer>
+    </>
+  );
+}
+
+export default App;
---
+++ Frontend/src/routers/router.jsx
@@ -0,0 +1,21 @@
+import { createBrowserRouter } from "react-router-dom";
+import App from "../App";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <div>Home</div>,
+      },
+      {
+        path: "/order",
+        element: <div>order</div>,
+      },
+    ],
+  },
+]);
+
+export default router;
