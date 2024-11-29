99d8ddbe199fd1039156f74787c16d2342de2618
aakashstha1 <aakash.cerestha90@gmail.com>
2024-11-29 13:39:35 +0545

    ninth commit

--- Backend/src/books/book.controller.js
+++ Backend/src/books/book.controller.js
@@ -1,18 +1,78 @@
 const Book = require("./book.model");
 
+//Post a book
+
 const postABook = async (req, res) => {
   try {
     const newBook = new Book(req.body); // Use the Mongoose model to create a new book
     await newBook.save();
-    res
-      .status(200)
-      .send({ message: "Book posted successfully", book: newBook });
+    res.status(200).send({
+      success: true,
+      message: "Book posted successfully",
+      book: newBook,
+    });
   } catch (error) {
     console.error(error);
-    res.status(500).send({ message: "Failed to create book", error });
+    res
+      .status(500)
+      .send({ success: false, message: "Failed to create book", error });
+  }
+};
+
+//Get all books
+const getAllBooks = async (req, res) => {
+  try {
+    const books = await Book.find().sort({ createdAt: -1 });
+    res.status(200).send({ success: true, book: books });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ success: false, message: "Failed to fetch books" });
+  }
+};
+
+//Get single book
+const getSingleBook = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const book = await Book.findById(id);
+    if (!book) {
+      res.status(404).send({ success: false, message: "Book not found!" });
+    }
+    res.status(200).send({ success: true, book: book });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ success: false, message: "Failed to fetch book" });
+  }
+};
+
+//Update a book
+const updateBook = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const updatedBook = await Book.findByIdAndUpdate(id, req.body, {
+      new: true,
+    });
+    if (!updatedBook) {
+      res.status(404).send({ success: false, message: "Book not found!" });
+    }
+    res
+      .status(200)
+      .send({
+        success: true,
+        message: "Book updated successfully",
+        book: updateBook,
+      });
+  } catch (error) {
+    console.log(error);
+    res
+      .status(500)
+      .send({ success: false, message: "Failed to update a book" });
   }
 };
 
 module.exports = {
   postABook,
+  getAllBooks,
+  getSingleBook,
+  updateBook,
 };
--- Backend/src/books/book.route.js
+++ Backend/src/books/book.route.js
@@ -2,9 +2,23 @@ const express = require("express");
 const router = express.Router();
 const Book = require("./book.model");
 
-const { postABook } = require("./book.controller");
+const {
+  postABook,
+  getAllBooks,
+  getSingleBook,
+  updateBook,
+} = require("./book.controller");
 
 //Post a book
 router.post("/create-book", postABook);
+
+//Get all books
+router.get("/", getAllBooks);
+
+//Get a single book
+router.get("/:id", getSingleBook);
+
+//Update a book
+router.put("/edit/:id", updateBook);
 
 module.exports = router;
